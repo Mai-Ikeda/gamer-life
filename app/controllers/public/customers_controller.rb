38b00e312d600d326c24f45d1fe3b0d2a4651ea5
Mai-Ikeda <126255405+Mai-Ikeda@users.noreply.github.com>
2023-08-27 11:39:57 +0000

    Add about

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!, except:[:index]
   
   def index
     @customers = Customer.all.page(params[:page]).per(10)
