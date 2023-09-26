0c9a9f44ef2b24dd4c9554d73f871c0eff1a4f88
Mai-Ikeda <126255405+Mai-Ikeda@users.noreply.github.com>
2023-09-26 20:42:45 +0000

    Fix コントローラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   
   protected
   
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_top_path
+    when Customer
+      root_path
+    end
+  end
+    
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
   end
