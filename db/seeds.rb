ccf2c95954193a11e4750723aa16f2d09f409705
Mai-Ikeda <126255405+Mai-Ikeda@users.noreply.github.com>
2023-10-12 12:02:38 +0000

    Add test date

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,62 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(email: 'admin@test', password: 'admintest')
+Admin.create!(email: 'admin@test', password: 'admintest')
+
+user1 = Customer.find_or_create_by!(email: "user1@xxx") do |customer|
+  customer.user_name = "User1"
+  customer.password = "password"
+  customer.like_game = "アクション、アドベンチャーゲームが好きです。"
+end
+
+user2 = Customer.find_or_create_by!(email: "user2@xxx") do |customer|
+  customer.user_name = "User2"
+  customer.password = "password"
+  customer.like_game = "シミュレーションゲームにはまっています。"
+end
+
+Impression.create!(
+  [
+    {
+      customer_id: 2,
+      genre_id: 1,
+      title: "ポケットモンスター緑",
+      body: "私が初めてプレイしたゲームです。最初のジムリーダーに勝てなくて何度も挑戦したのはいい思い出です。",
+      rate: 4,
+      status: 0,
+    }
+  ]
+)
+
+Genre.create!(
+  [
+    {
+      name: "アクション",
+      is_active: true,
+    },
+    {
+      name: "アドベンチャー",
+      is_active: true,
+    },
+    {
+      name: "シミュレーション",
+      is_active: true,
+    },
+    {
+      name: "RPG",
+      is_active: true,
+    },
+    {
+      name: "シューティング",
+      is_active: true,
+    },
+    {
+      name: "レーシング",
+      is_active: true,
+    },
+    {
+      name: "その他",
+      is_active: true,
+    }
+  ]
+)
